901ce15c7bbc898c292640046f60aac8b1464aec
Robert King <varitonium@gmail.com>
2026-03-26 21:49:05 +0100

    XCR-17: upe-adding of unit test for XcurlCtx class

--- test/test_xcgen.cpp
+++ test/test_xcgen.cpp
@@ -5,13 +5,67 @@
 
 #include <string>
 
-#include <xcurl.h>
-#include <xcurl_ctx.h>
-#include <xcurl_global.h>
+#include <xcurl>
+#include <curl/curl.h>
+
+using Xcurl_ctx =
+  xcgen::XcurlCtx<xcwpr::Xcurl, std::string, xcwpr::XcurlGlobal>;
 
 TEST_CASE("XCurlCtx")
 {
-  using namespace xcwpr;
-  xcgen::XcurlCtx<Xcurl, std::string, XcurlGlobal>{"https://httpbin.org/get"};
-  CHECK(true);
+  Xcurl_ctx cctx{"https://httpbin.org/get"};
+  CHECK(cctx.ctx_status());
+  CHECK(cctx->http_code() == HTTP_NOT_SET);
+  CHECK(cctx->status() == CURLE_OK);
+}
+
+TEST_CASE("read_buffer empty")
+{
+  Xcurl_ctx cctx{"https://httpbin.org/get"};
+  CHECK(cctx.ctx_status());
+  CHECK(cctx->read_buffer().empty());
+  CHECK(cctx->status() == CURLE_OK);
+  CHECK(cctx->http_code() == HTTP_NOT_SET);
+  CHECK(cctx.ctx_status());
+}
+
+TEST_CASE("functor [read_buffer empty]")
+{
+  Xcurl_ctx cctx{"https://httpbin.org/get"};
+  CHECK(cctx.ctx_status());
+  CHECK(cctx->read_buffer().empty());
+  CHECK(cctx->status() == CURLE_OK);
+  CHECK(cctx->http_code() == HTTP_NOT_SET);
+}
+
+TEST_CASE("read_buffer with real url")
+{
+  Xcurl_ctx cctx{"https://httpbin.org/get"};
+  CHECK(cctx.ctx_status());
+  auto ret = cctx->fetch_data();
+  CHECK(ret);
+  CHECK(!cctx->read_buffer().empty());
+  CHECK(cctx->status() == CURLE_OK);
+  CHECK(cctx->http_code() == 200);
+}
+
+TEST_CASE("read_buffer with fake url")
+{
+  Xcurl_ctx cctx{"https://xspectrum/get"};
+  CHECK(cctx.ctx_status());
+  auto ret = cctx->fetch_data();
+  CHECK(!ret);
+  CHECK(cctx->read_buffer().empty());
+  CHECK(cctx->http_code() == HTTP_NOT_SET);
+}
+
+TEST_CASE("read_buffer with malformat url")
+{
+  Xcurl_ctx cctx{"1Z%"};
+  CHECK(cctx.ctx_status());
+  auto ret = cctx->fetch_data();
+  CHECK(!ret);
+  CHECK(cctx->read_buffer().empty());
+  CHECK(cctx->status() == CURLE_URL_MALFORMAT);
+  CHECK(cctx->http_code() == HTTP_NOT_SET);
 }
