889e4630a023763b38bbdf8cd488e7c89e03a1e7
Robert King <varitonium@gmail.com>
2026-04-04 00:25:12 +0200

    XCR-32: dev-adding of non copyable template class

--- test/test_utils.cpp
+++ test/test_utils.cpp
@@ -4,17 +4,31 @@
 #include <doctest/doctest.h>
 
 #include <ncnm.h>
+#include <nc.h>
 #include <utils>
 
 template <typename T>
 using Default = utils::NCNM<T>;
 
+template <typename T>
+using Movable = utils::NC<T>;
+
 struct Sample : public Default<Sample>
+{
+};
+
+struct Decoy : public Movable<Decoy>
 {
 };
 
 TEST_CASE("Non copyble non moveable class")
 {
   static_assert(topology::utils::NonCopyableNonMovable<Sample>);
+  CHECK(true);
+}
+
+TEST_CASE("Non copyble class")
+{
+  static_assert(topology::utils::NonCopyable<Decoy>);
   CHECK(true);
 }
