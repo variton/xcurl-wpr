e077f1410e71981005766a5d117ef6a75c7634fe
Robert King <varitonium@gmail.com>
2026-04-14 23:28:30 +0200

    XCR-42: dev-adding of EnvMgr class

---
+++ test/test_platform.cpp
@@ -0,0 +1,22 @@
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <doctest/doctest.h>
+
+#include <env_mgr.h>
+
+TEST_CASE("Get_env_var")
+{
+  auto rc = platform::EnvMgr::get_instance().get_env_var("RC");
+  if (rc) {
+    CHECK(*rc == "/home/cxx-core/rc");
+  }
+}
+
+TEST_CASE("Get_env_var fail")
+{
+  auto rc = platform::EnvMgr::get_instance().get_env_var("X");
+  if (!rc) {
+    CHECK(true);
+  }
+}
