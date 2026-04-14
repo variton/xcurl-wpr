ed154870e8981515ca8a2460eab83b219fe7f6bc
Robert King <varitonium@gmail.com>
2026-04-15 00:25:34 +0200

    XCR-44: upe-adding of get_envv

--- test/test_platform.cpp
+++ test/test_platform.cpp
@@ -20,3 +20,22 @@ TEST_CASE("EnvMgr get_env_var fail")
     CHECK(true);
   }
 }
+
+TEST_CASE("EnvMgr get_envv")
+{
+  auto &local = platform::EnvMgr::get_instance();
+  auto rc = local.get_envv("RC");
+  if (rc) {
+    CHECK(rc.value() == "/home/cxx-core/rc");
+  }
+}
+
+TEST_CASE("EnvMgr get_envv fail")
+{
+  auto &local = platform::EnvMgr::get_instance();
+  auto rc = local.get_envv("RC");
+  if (!rc) {
+    CHECK(rc.error().type == platform::EnvMgrError::NoEnvVar);
+    CHECK(rc.error().message == "No env var found\n");
+  }
+}
